c902b23272d211c413b2ca1827d49b135037f6a5
Pedro Javier Avila Navarro <pedjavavinav64@hotmail.com>
2026-04-14 20:09:54 +0200

    Initial commit

---
+++ src/test/java/package01/TestPrueba.java
@@ -0,0 +1,12 @@
+package package01;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestPrueba {
+	  @Test
+	    void testExample() {
+	        assertEquals(2, 1 + 1);
+	    }
+}
